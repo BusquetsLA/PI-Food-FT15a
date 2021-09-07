351fd5840da95c8dc50232e248d5003bd1395da7
BusquetsLA <busquetsla@gmail.com>
2021-09-07 09:52:57 -0300

    modificados reducers en gral

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,62 +1,67 @@
-import { GET_RECIPES, GET_BY_NAME, GET_RECIPE_DETAIL, CLEAR_RECIPE_DETAIL, ORDER_RECIPES, GET_DIETS, FILTER_DIET/*, CREATE_RECIPE*/ } from '../actions/index';
-import { orderByParam, filterByDiet } from '../controllers/index';
+import {
+  GET_RECIPES,
+  GET_BY_NAME,
+  GET_RECIPE_DETAIL,
+  CLEAR_RECIPE_DETAIL,
+  ORDER_RECIPES,
+  GET_DIETS,
+  FILTER_DIET /*, CREATE_RECIPE*/,
+} from "../actions/index";
+import { orderByParam, filterByDiet } from "../controllers/index";
 
 const initialState = {
-    recipes: [], // todos los recipes cargados de getRecipes
-    diets: [],
-    recipeDetails: [],
-    filteredRecipes: [],
-    createdRecipe: [],
+  recipes: [], // todos los recipes cargados de getRecipes
+  diets: [],
+  recipeDetails: [],
+  filteredRecipes: [],
 };
-function rootReducer (state = initialState, action) {
-    switch (action.type) {
-        // RECIPES
-        case GET_RECIPES:
-            return {
-                ...state,
-                recipes: action.payload,
-                filteredRecipes:action.payload
-            }
-        case GET_BY_NAME:
-            return {
-                ...state,
-                recipes: action.payload,
-            };
-        case GET_RECIPE_DETAIL:
-            return {
-                ...state,
-                recipeDetails: action.payload
-            };
-        case CLEAR_RECIPE_DETAIL:
-            return {
-                ...state,
-                recipeDetails: action.payload
-            };
-        // ORDER
-        case ORDER_RECIPES:
-            return {
-                ...state,
-                recipes: orderByParam(action.payload, state.filteredRecipes)
-            };
-        // DIETS
-        case GET_DIETS:
-            return {
-                ...state,
-                diets: action.payload
-            };
-        case FILTER_DIET:
-            return {
-                ...state,
-                recipes: filterByDiet(action.payload, state.filteredRecipes)
-            };
-        // CREATE
-        /*case CREATE_RECIPE:
-            return{
-                ...state,
-                createdRecipe: action.payload
-            };*/
-        default:
-            return state;
-    };
-};
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    // RECIPES
+    case GET_RECIPES:
+      return {
+        ...state,
+        recipes: action.payload,
+        // filteredRecipes: action.payload
+      };
+    case GET_BY_NAME:
+      return {
+        ...state,
+        recipes: action.payload,
+      };
+    case GET_RECIPE_DETAIL:
+      return {
+        ...state,
+        recipeDetails: action.payload,
+      };
+    case CLEAR_RECIPE_DETAIL:
+      return {
+        ...state,
+        recipeDetails: action.payload,
+      };
+    // ORDER
+    case ORDER_RECIPES:
+      return {
+        ...state,
+        recipes: orderByParam(action.payload, state.filteredRecipes),
+      };
+    // DIETS
+    case GET_DIETS:
+      return {
+        ...state,
+        diets: action.payload,
+      };
+    case FILTER_DIET:
+      return {
+        ...state,
+        recipes: filterByDiet(action.payload, state.filteredRecipes),
+      };
+    case CREATE_RECIPE:
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
+}
 export default rootReducer;
